88043c621d3ecc7450d0161854adc3551d5e0e6a
DESKTOP-JH6KN26\Rosevil <15773111424@163.com>
2018-07-08 14:07:34 +0800

    532修改

--- Python-Solution/532_K-diff-Pairs-in-an-Array/532.py
+++ Python-Solution/532_K-diff-Pairs-in-an-Array/532.py
@@ -1,27 +1,16 @@
 class Solution:
-    def findPairs(self, nums, k):
+    def findDuplicates(self, nums):
         """
         :type nums: List[int]
-        :type k: int
-        :rtype: int
+        :rtype: List[int]
         """
-        cnt = 0
-        nums_set = set(nums)     #自动排序、去重
-        if k < 0:
-            return 0
-        elif k == 0:
-            for x in nums_set:
-                if nums.count(x) > 1:
-                    cnt += 1
-        else:
-            for x in nums_set:
-                if x + k in nums_set:
-                    cnt += 1
-        return cnt
+        res = []
 
+        for x in nums:
+        	nums[x - 1] = - nums[x - 1]
 
-        
-nums = [3, 1, 4, 1, 5]
-s = Solution()
-res = s.findPairs(nums, 2)
-print(res)
+        for i in range(len(nums)):
+        	if nums[i] >= 0:
+        		res.append(i + 1)
+
+        return res
