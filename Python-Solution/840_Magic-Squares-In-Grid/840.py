2a0e7102a1f68e24e12d771b9ffee15ee67a7188
Saumen <saumenroy323@gmail.com>
2019-03-01 08:07:13 +0600

    Update 840.py

--- Python-Solution/840_Magic-Squares-In-Grid/840.py
+++ Python-Solution/840_Magic-Squares-In-Grid/840.py
@@ -1,45 +1,19 @@
-import itertools
-class Solution:
+class Solution(object):
     def numMagicSquaresInside(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        cnt = 0
-        height, width = len(grid), len(grid[0])
-        if height < 3 or width < 3:
-            return 0
+        R, C = len(grid), len(grid[0])
 
+        def magic(a,b,c,d,e,f,g,h,i):
+            return (sorted([a,b,c,d,e,f,g,h,i]) == range(1, 10) and
+                (a+b+c == d+e+f == g+h+i == a+d+g ==
+                 b+e+h == c+f+i == a+e+i == c+e+g == 15))
 
-        for i in range(height - 2):
-            for j in range(width - 2):
-                matrix = [ row[ j : j + 3] for row in grid[ i : i + 3]]
-                if any(x < 1 or x > 9 for x in itertools.chain(*matrix)):
-                    continue
-
-                # 计算3*3矩阵中每一行的和
-                rows = { sum( matrix[i] ) for i in range(3) }
-
-                # 计算3*3矩阵中每一列的和
-                T = list( itertools.zip_longest(*matrix) )
-                cols = { sum( T[i] ) for i in range(3) }
-
-                # 计算3*3矩阵两条对角线的和
-                diagonal1 = { sum(matrix[i][i] for i in range(3)) }
-                diagonal2 = { sum(matrix[~i][~i] for i in range(3)) }
-
-                # 判断以上计算的三种和是否相等
-                if len( rows | cols | diagonal1 | diagonal2 ) == 1:
-                    cnt += 1
-
-        return cnt
-                
-
-     
-grid = [[4,3,8,4],
-        [9,5,1,9],
-        [2,7,6,2]]
-s = Solution()
-r = s.numMagicSquaresInside(grid)
-print(r)
+        ans = 0
+        for r in xrange(R-2):
+            for c in xrange(C-2):
+                if grid[r+1][c+1] != 5: continue  # optional skip
+                if magic(grid[r][c], grid[r][c+1], grid[r][c+2],
+                         grid[r+1][c], grid[r+1][c+1], grid[r+1][c+2],
+                         grid[r+2][c], grid[r+2][c+1], grid[r+2][c+2]):
+                    ans += 1
+        return ans
     
